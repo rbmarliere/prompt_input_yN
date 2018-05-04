b12c3cbf9e11e7c55a6a7a70b0b85a85a581680b
zaratustra <zrts@protonmail.com>
2018-05-04 11:55:24 -0300

    prompt_input_yN

---
+++ prompt_input_yN.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+prompt_input_yN()
+{
+    printf "$1? [y|N] " ; shift
+    while true; do
+        read YN
+        case ${YN} in
+            [Yy]* ) printf "\n"; return 0; break;;
+            * ) printf "\n"; return 1; break;;
+        esac
+    done
+}
+
